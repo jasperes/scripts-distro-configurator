0d06e21975586fff2a949fa83d8348ae2eb6ffee
Jonathan Peres <jonathan.peres@protonmail.com>
2017-03-02 15:12:16 -0300

    Add programs version in configuration file

--- configuration.sh
+++ configuration.sh
@@ -6,3 +6,10 @@ export JDK_BIN=$JDK_PATH/bin/java
 export EXTRACT_PATH=~/tmp_dist_cfg
 export PKGS_PATH=/media/user/storage/linux23
 
+export JDK7_VERSION=7u80
+export JDK8_VERSION=8u112
+export ECLIPSE_VERSION=mars-2
+export SQLDEVELOPER_VERSION=4.1.5.21.78
+export JDTS_VERSION=3.1.3
+export MYSQL_JDBC_VERSION=4.1.40
+
--- install.sh
+++ install.sh
@@ -12,27 +12,27 @@ cd dirname
 ./configuration.sh
 
 # JDK
-sudo tar -xzvf java/jdk-7u80-linux-i586.tar.gz -C $JAVA_PATH
-sudo tar -xzvf java/jdk-8u112-linux-i586.tar.gz -C $JAVA_PATH
+sudo tar -xzvf java/jdk-$JDK7_VERSION-linux-i586.tar.gz -C $JAVA_PATH
+sudo tar -xzvf java/jdk-$JDK8_VERSION-linux-i586.tar.gz -C $JAVA_PATH
 
 # Eclipse
-tar -xzvf ide/eclipse-cpp-mars-2-linux-gtk.tar.gz -C $EXTRACT_PATH
+tar -xzvf ide/eclipse-cpp-$ECLIPSE_VERSION-linux-gtk.tar.gz -C $EXTRACT_PATH
 sudo mv $EXTRACT_PATH/eclipse /opt/eclipse-cpp
 python scripts/update_eclipse_ini.py /opt/eclipse-cpp/eclipse.ini $JDK_BIN
 
-tar -xzvf ide/eclipse-jee-mars-2-linux-gtk.tar.gz -C $EXTRACT_PATH
+tar -xzvf ide/eclipse-jee-$ECLIPSE_VERSION-linux-gtk.tar.gz -C $EXTRACT_PATH
 sudo mv $EXTRACT_PATH/eclipse /opt/eclipse-jee
 python scripts/update_eclipse_ini.py /opt/eclipse-jee/eclipse.ini $JDK_BIN
 
-tar -xzvf ide/eclipse-java-mars-2-linux-gtk.tar.gz -C $EXTRACT_PATH
+tar -xzvf ide/eclipse-java-$ECLIPSE_VERSION-linux-gtk.tar.gz -C $EXTRACT_PATH
 sudo mv $EXTRACT_PATH/eclipse /opt/eclipse-java
 python scripts/update_eclipse_ini.py /opt/eclipse-java/eclipse.ini $JDK_BIN
 
 # SQLDeveloper
-unzip sqldeveloper/sqldeveloper-4.1.5.21.78-no-jre.zip -d $EXTRACT_PATH
+unzip sqldeveloper/sqldeveloper-$SQLDEVELOPER_VERSION-no-jre.zip -d $EXTRACT_PATH
 sudo mv $EXTRACT_PATH/sqldeveloper /opt/sqldeveloper
-cp sqldeveloper/libs/jtds-1.3.1.jar /opt/sqldeveloper/jdbc/lib/
-cp sqldeveloper/libs/mysql-connector-java-5.1.40-bin.jar /opt/sqldeveloper/jdbc/lib/
+cp sqldeveloper/libs/jtds-$JDTS_VERSION.jar /opt/sqldeveloper/jdbc/lib/
+cp sqldeveloper/libs/mysql-connector-java-$MYSQL_JDBC_VERSION-bin.jar /opt/sqldeveloper/jdbc/lib/
 python scripts/update_sqldeveloper_jdk.py /opt/sqldeveloper/sqldeveloper/bin/sqldeveloper.conf $JDK_PATH
 
 # Clean
