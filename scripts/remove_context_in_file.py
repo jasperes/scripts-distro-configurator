b27722cc28f21c4b7c90b193671a2bda502b86a0
Jonathan Peres <jonathan.peres@protonmail.com>
2017-03-22 22:28:33 -0300

    add openbox menu configuration for new softwares

---
+++ scripts/remove_context_in_file.py
@@ -0,0 +1,22 @@
+import sys
+
+file_name = sys.argv[1]
+context = sys.argv[2]
+
+start = ("<!-- :%s:" % context)
+final = (":%s: -->" % context)
+
+text = []
+
+print("Removing context with %s in file %s" % (context, file_name))
+
+with open(file_name, 'r') as f:
+    for line in f.readlines():
+        if start not in line and final not in line:
+            text.append(line)
+
+with open(file_name, 'w') as f:
+    print("Updating file...")
+    f.writelines(text)
+
+print("File updated with sucess!\n")
