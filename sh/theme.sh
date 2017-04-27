65cf64258b003df00dff086c9a8538a4b8437b95
Jonathan Peres <jonathan.peres@protonmail.com>
2017-04-26 22:10:39 -0300

    clean old theme first, then update

--- sh/theme.sh
+++ sh/theme.sh
@@ -3,17 +3,21 @@
 function theme_install {
 
     ## Install GTK2, GTK3, Openbox themes
+    rm -rf ~/.local/share/themes/*
     cp -r $RESOURCES/themes/* ~/.local/share/themes
 
     ## Install fonts
+    rm -rf ~/.local/share/fonts/*
     cp -r $RESOURCES/fonts/* ~/.local/share/fonts
     fc-cache -f -v
 
     ## Tint2 theme
-    cp $RESOURCES/tint2/* ~/.config/tint2
+    rm -rf ~/.config/tint2/*
+    cp -r $RESOURCES/tint2/* ~/.config/tint2
 
     ## Conky
-    cp $RESOURCES/conky/* ~/.config/conky
+    rm -rf ~/.config/conky/*
+    cp -r $RESOURCES/conky/* ~/.config/conky
     
 }
 
