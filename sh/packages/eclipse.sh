3484829ad2069c12bd33138f2b9214ccfe57493a
Jonathan Peres <jonathan.peres@mykolab.com>
2017-06-19 01:44:20 -0300

    Update eclipse script

--- sh/packages/eclipse.sh
+++ sh/packages/eclipse.sh
@@ -1,40 +1,38 @@
 #!/bin/bash
 
 function eclipse() {
-    OPT_PATH=/opt/eclipse-java
-
-    # curl http://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/mars/2/eclipse-java-mars-2-linux-gtk-x86_64.tar.gz
-    tar -xzvf ide/eclipse-java-$ECLIPSE_VERSION-linux-gtk.tar.gz -C $TMP_PATH
-    sudo mv $TMP_PATH/eclipse $OPT_PATH
-
-    Python scripts/update_eclipse_ini.py $OPT_PATH/eclipse.ini $JDK_BIN
-    python scripts/remove_context_in_file $OPENBOX_MENU_PATH ECLIPSE
-
-    _create_run $OPT_PATH "./eclipse" "eclipse"
+    _run_eclipse java
 }
 
 function eclipse_jee() {
-    OPT_PATH=/opt/eclipse-jee
-
-    # curl http://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/mars/2/eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz
-    tar -xzvf ide/eclipse-jee-$ECLIPSE_VERSION-linux-gtk.tar.gz -C $TMP_PATH
-    sudo mv $TMP_PATH/eclipse $OPT_PATH
-    
-    python scripts/update_eclipse_ini.py $OPT_PATH/eclipse.ini $JDK_BIN
-    python scripts/remove_context_in_file $OPENBOX_MENU_PATH ECLIPSE_JEE
-
-    _create_run $OPT_PATH "./eclipse" "eclipse-jee"
+    _run_eclipse jee
 }
 
 function eclipse_cpp() {
-    OPT_PATH=/opt/eclipse-cpp
+    _run_eclipse cpp
+}
 
-    #curl http://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/mars/2/eclipse-cpp-mars-2-linux-gtk-x86_64.tar.gz
-    tar -xzvf ide/eclipse-cpp-$ECLIPSE_VERSION-linux-gtk.tar.gz -C $TMP_PATH
+function _run_eclipse() {
+    OPT_PATH=/opt/eclipse-$1
+
+    if $(is_to_download eclipse_$1); then
+        _download_eclipse $1
+    fi;
+
+    tar -xzvf $PATH_PACKAGES/eclipse-$1-$ECLIPSE_VERSION-$ECLIPSE_RELEASE-linux-gtk-$ARCH.tar.gz -C $TMP_PATH
     sudo mv $TMP_PATH/eclipse $OPT_PATH
 
-    python scripts/update_eclipse_ini.py $OPT_PATH/eclipse.ini $JDK_BIN
-    python scripts/remove_context_in_file $OPENBOX_MENU_PATH ECLIPSE_CPP
+    Python scripts/update_eclipse_ini.py $OPT_PATH/eclipse.ini $JDK_BIN
 
-    _create_run $OPT_PATH "./eclipse" "eclipse-cpp"
+    if [[ $WM == 'openbox' ]]; then
+        python scripts/remove_context_in_file $OPENBOX_MENU_PATH $(echo eclipse-$2 | awk '{print toupper($0)}')
+    fi;
+
+    create_run $OPT_PATH "./eclipse" "eclipse-$1"
+}
+
+function _download_eclipse() {
+    cd $PATH_PACKAGES &&
+    { curl -O "http://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/${ECLIPSE_VERSION}/${ECLIPSE_RELEASE}/eclipse-${1}-${ECLIPSE_VERSION}-${ECLIPSE_RELEASE}-linux-gtk-${ARCH}.tar.gz" ;
+    cd -; }
 }
