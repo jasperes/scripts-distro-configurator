2baea09467abb691fd4ba420fb4490432be0898b
Jonathan Peres <jonathan.peres@mykolab.com>
2017-06-27 12:03:50 -0300

    Fix execute script

--- sh/init/execute.sh
+++ sh/init/execute.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 function execute() {
+
 	# Install all distro dependencies
 	distro_install_dependencies
 
 	# Execute each packages scripts
-	exec_funcs PACKAGES
+	exec_funcs $PACKAGES
 
 }
