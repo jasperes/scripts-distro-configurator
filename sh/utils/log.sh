d1bc019c1ba0394ade811cf93a99dcd807c9eb8e
Jonathan Peres <jonathan.peres@mykolab.com>
2017-06-28 00:08:28 -0300

    Add log script

--- sh/utils/log.sh
+++ sh/utils/log.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-function log() {
+function _log() {
+    echo "$1" | tee -a "$PATH_LOG/$LOG_FILE"
+}
+
+function log_info() {
+
+    if [ ! -f "$PATH_LOG/$LOG_FILE" ]; then
+        touch "$PATH_LOG/$LOG_FILE"
+        _log "Log file CREATED: $(date)"
+    fi
+
+    _log "$1"
 
 }
 
 function verbose() {
-	log()
+    if [[ "$VERBOSE" == "true" ]]; then
+	   log_info "$1"
+    fi
 }
